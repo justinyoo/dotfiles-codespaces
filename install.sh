43442d3d4c8a7cc7e58dba98e9f7a4a80689b121
Justin Yoo <justin.yoojh@gmail.com>
2022-01-18 19:33:35 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 echo "[$(date +"%Y-%m-%d %H:%M:%S")] Installing dotfiles ..."
 
+## Set environment variables
+
+export CODESPACES_REPOSITORY=/workspaces/$RepositoryName
+
 ## Copy oh-my-zsh files
 mv ~/.zshrc ~/.zshrc.bak
 cp .zshrc ~/

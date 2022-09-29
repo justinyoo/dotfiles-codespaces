e0401d13b47d7b33c399a5d1a5135e8baee597e3
Justin Yoo <justin.yoojh@gmail.com>
2022-09-29 22:22:36 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,15 +6,17 @@ echo "[$(date +"%Y-%m-%d %H:%M:%S")] Installing dotfiles ..."
 
 export CODESPACES_REPOSITORY=/workspaces/$RepositoryName
 
-# Update the oh-my-zsh settings without your dotfiles
+# Update the oh-my-zsh settings
 cp .p10k-with-clock.zsh ~/
 cp .p10k-without-clock.zsh ~/
 cp switch-p10k-clock.sh ~/
 
-mv ~/.zshrc ~/.zshrc.bak
+cp ~/.p10k-with-clock.zsh ~/.p10k.zsh
+cp ~/.zshrc ~/.zshrc.bak
 
-echo "$(sed 's/ZSH_THEME="codespaces"/ZSH_THEME="powerlevel10k"/g' ~/.zshrc)" > ~/.zshrc
-echo "$(sed 's/plugins=(git)/plugins=(git zsh-completions zsh-syntax-highlighting zsh-autosuggestions)/g' ~/.zshrc)" > ~/.zshrc
+echo "$(cat $HOME/.zshrc)" | awk '{gsub(/ZSH_THEME=\"codespaces\"/, "ZSH_THEME=\"powerlevel10k\"")}1' > $HOME/.zshrc.replaced && mv $HOME/.zshrc.replaced $HOME/.zshrc
+echo "$(cat $HOME/.zshrc)" | awk '{gsub(/plugins=\(git\)/, "plugins=(git zsh-completions zsh-syntax-highlighting zsh-autosuggestions)")}1' > $HOME/.zshrc.replaced && mv $HOME/.zshrc.replaced $HOME/.zshrc
 echo "
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh" >> $HOME/.zshrc
+# To customize prompt, run 'p10k configure' or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+" >> $HOME/.zshrc
